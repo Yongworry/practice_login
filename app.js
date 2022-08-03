eb97b554d1f6d29762b6b831274b0fc8ad150dbc
박용원 <pak-yong-won@bag-yong-won-ui-MacBookAir.local>
2022-08-03 22:34:19 +0900

    깃 저장소 초기화

---
+++ app.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+
+
+//라우팅
+const home = require("./routes/home");
+
+//앱 세팅
+app.set("views", "./views");
+app.set("view engine", "ejs"); 
+
+app.use("/", home); //use는 미들웨어 쓸때 쓴다
+
+module.exports = app;
+
